20249c4cb59a388d65dfe34fdd5b5300566651e7
Zeerak Asim <Z_zee09@hotmail.com>
2024-07-24 04:05:33 -0400

    init
    added files and slowly doing stuff

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black text-white">
+      <div>
+        <h1>Zeerak Asim</h1>
+        <h2>Recent Computer Engineering Graduate</h2>
+      </div>
+    </main>
+  );
+}
