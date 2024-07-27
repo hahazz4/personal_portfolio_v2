caf1f6b81405b8015bc1c186183054554760e89c
Zeerak Asim <72907332+hahazz4@users.noreply.github.com>
2024-07-27 01:56:15 -0400

    Delete CNAME & changed stuff for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/personal_portfolio_v2' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/personal_portfolio_v2' : '',
-    images:{
-        unoptimized: true,
-    },
-    trailingSlash: true,
     output: 'export',
+    images: {
+      unoptimized: true,
+    },
 };
-
+  
 export default nextConfig;
+  
