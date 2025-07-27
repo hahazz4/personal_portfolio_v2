84eea5874668a2090cc858e356c963962e150a0e
z.asim <Z_zee09@hotmail.com>
2025-07-27 03:42:47 -0400

    anotha update to website & resume

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,14 +5,35 @@ import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
 
 const data = [
   {
-    src: '/images/headstarter_img.png',
-    title: 'Headstarter AI Projects Portfolio',
-    description: 'Headstarter Portfolio with 4+ Project Submissions',
-    category: 'Fellowship @ Headstarter',
-    content: `Built 4+ applications with AI using Next js, Tailwind CSS, Material UI, Node.js, TypeScript, JavaScript.
-    Incorporated LLM integration with methods like RAG, APIs like Gemini, OpenAI API, and much more...`,
-    website: 'https://app.theheadstarter.com/profile/zeerakasim'
+    src: '/images/FlashcardGenProject.png',
+    title: 'Flashcard-Gen',
+    description: 'Study Cards Generator',
+    category: 'Project',
+    content: 'Designed a flashcard app using Next.js, Tailwind CSS, Material UI, and JavaScript, with the back-end as Node.js, and LLM integration through the Gemini API.',
+    repository: 'https://github.com/hahazz4/ai_flashcard',
+    website: 'https://ai-flashcard-rosy.vercel.app/',
+    videoURL: "/FlashcardGenProject.mov",
   },
+  {
+    src: '/images/ai-olympic-project.png',
+    title: 'AI Olympic Bot',
+    description: 'Chatbot For Olympics',
+    category: 'Project',
+    content: 'Developed an AI bot that answers Olympic FAQS using Next.js, Material UI, JavaScript, and integrated LLM using the Gemini API',
+    repository: 'https://github.com/hahazz4/ai_supp_bot',
+    website: 'http://ai-supp-bot.vercel.app/',
+    videoURL: "/olympic-bot.mp4",
+  },
+  // what they doin myan? client-side error? ;-;
+  // {
+  //   src: '/images/headstarter_img.png',
+  //   title: 'Headstarter AI Projects Portfolio',
+  //   description: 'Headstarter Portfolio with 4+ Project Submissions',
+  //   category: 'Fellowship @ Headstarter',
+  //   content: `Built 4+ applications with AI using Next js, Tailwind CSS, Material UI, Node.js, TypeScript, JavaScript.
+  //   Incorporated LLM integration with methods like RAG, APIs like Gemini, OpenAI API, and much more...`,
+  //   website: 'https://app.theheadstarter.com/profile/zeerakasim'
+  // },
   {
     src: '/images/gunkii_sc.png',
     title: 'Gunkii Shopping Cart UI',
@@ -22,13 +43,12 @@ const data = [
     videoURL: "/gunkii_shopping_cart_demo.mp4",
   },
   {
-    src: '/images/old_portfolio.png',
-    title: 'Old Portfolio Website (2022-2024)',
-    description: 'My old personal portfolio',
-    category: 'Portfolio (Project)',
-    content: 'Made using HTML, CSS, Bootstrap, and Javascript. Displays my reusme, who I am, my projects, and my socials.',
-    repository: 'https://github.com/hahazz4/My_Personal_Portfolio',
-    website: 'https://hahazz4.github.io/My_Personal_Portfolio/'
+      src: '/images/memory_alloc_project.png',
+      title: 'Memory Allocator',
+      description: `A Memory Allocator in C++`,
+      category: 'Project',
+      content: 'A memory allocator simulator that was made to mimic and display the behaviour of allocating and de-allocating blocks of memory (malloc(), free()).',
+      repository: 'https://github.com/hahazz4/Memory-Allocator-Simulation',
   },
   {
     src: '/images/RecipeHuntAppPic.png',
@@ -57,43 +77,15 @@ const data = [
     repository: 'https://github.com/hahazz4/qhdt_sensors',
     videoURL: "/qhdt_year1project_demo.mp4",
   },
-  // Old Projects
-  // {
-  //   src: '/images/WeatherAppPic.png',
-  //   title: 'Weather App',
-  //   description: `A React Weather App`,
-  //   category: 'Project',
-  //   content: 'A React.js app that allows you to search a country or city, display its temperature and weather condition using the OpenWeatherMap API.',
-  //   repository: 'https://github.com/hahazz4/WeatherApp',
-  //   videoURL: "/weather_app_demo.mp4",
-  // },
-  // {
-  //   src: '/images/FibonacciTermAlgoApp.png',
-  //   title: 'Fibonacci Term App',
-  //   description: `A Netbeans Java Fibonacci Term App`,
-  //   category: 'Project',
-  //   content: 'Programmed using Java. Finds the number when given the position of the term for Fibonacci algorithm.',
-  //   repository: 'https://github.com/hahazz4/fibonacci_term_gui',
-  //   videoURL: "/fibonacci_term_finder_demo.mp4",
-  // },
-  // {
-  //   src: '/images/Todo List App.png',
-  //   title: 'Todo List App',
-  //   description: `A simple HTML,CSS,JS Todo List App`,
-  //   category: 'Project',
-  //   content: 'A simple todo list web app made with, HTML, CSS, Bootstrap and powered by JavaScript console.',
-  //   repository: 'https://github.com/hahazz4/Todo-List',
-  //   website: 'https://hahazz4.github.io/Todo-List/'
-  // },
-  // {
-  //   src: '/images/experimenting portfolio mockup 2021.png',
-  //   title: 'Portfolio Mockup (2021)',
-  //   description: `A Portfolio Mockup (2021)`,
-  //   category: 'Project',
-  //   content: 'A portfolio prototype I had programmed as a beginner using HTML and CSS.',
-  //   repository: 'https://github.com/hahazz4/Best_Website_I_Created',
-  //   website: 'https://hahazz4.github.io/Best_Website_I_Created/'
-  // },
+  {
+    src: '/images/old_portfolio.png',
+    title: 'Old Portfolio Website (2022-2024)',
+    description: 'My old personal portfolio',
+    category: 'Portfolio (Project)',
+    content: 'Made using HTML, CSS, Bootstrap, and Javascript. Displays my reusme, who I am, my projects, and my socials.',
+    repository: 'https://github.com/hahazz4/My_Personal_Portfolio',
+    website: 'https://hahazz4.github.io/My_Personal_Portfolio/'
+  },
 ];
 
 const cards = data.map((card, index) => (
